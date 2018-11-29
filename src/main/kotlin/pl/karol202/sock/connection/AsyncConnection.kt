86874f3ac9556a4127a258b7b74643ebb1acc79d
karol-202 <karoljurski1@gmail.com>
2018-11-29 21:44:24 +0100

    Don't use GlobalScope for coroutines.

--- src/main/kotlin/pl/karol202/sock/connection/AsyncConnection.kt
+++ src/main/kotlin/pl/karol202/sock/connection/AsyncConnection.kt
@@ -1,6 +1,7 @@
 package pl.karol202.sock.connection
 
 import com.github.kittinunf.result.Result
+import kotlinx.coroutines.CoroutineScope
 import java.io.IOException
 
 interface AsyncConnection
@@ -10,11 +11,11 @@ interface AsyncConnection
 		fun withCoroutines(connection: Connection) = CoroutinesAsyncConnection(connection)
 	}
 
-	fun connect(callback: (Result<Unit, Exception>) -> Unit)
+	fun connect(coroutineScope: CoroutineScope, callback: (Result<Unit, Exception>) -> Unit)
 
-	fun read(callback: (Result<ByteArray, IOException>) -> Unit)
+	fun read(coroutineScope: CoroutineScope, callback: (Result<ByteArray, IOException>) -> Unit)
 
-	fun send(data: ByteArray, callback: (Result<Unit, IOException>) -> Unit)
+	fun send(coroutineScope: CoroutineScope, data: ByteArray, callback: (Result<Unit, IOException>) -> Unit)
 
-	fun close(callback: (Result<Unit, IOException>) -> Unit)
+	fun close(coroutineScope: CoroutineScope, callback: (Result<Unit, IOException>) -> Unit)
 }
--- src/main/kotlin/pl/karol202/sock/connection/CoroutinesAsyncConnection.kt
+++ src/main/kotlin/pl/karol202/sock/connection/CoroutinesAsyncConnection.kt
@@ -1,36 +1,36 @@
 package pl.karol202.sock.connection
 
 import com.github.kittinunf.result.Result
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import java.io.IOException
 
 class CoroutinesAsyncConnection internal constructor(private val connection: Connection) : AsyncConnection
 {
-	override fun connect(callback: (Result<Unit, Exception>) -> Unit)
+	override fun connect(coroutineScope: CoroutineScope, callback: (Result<Unit, Exception>) -> Unit)
 	{
-		GlobalScope.launch {
+		coroutineScope.launch {
 			callback(Result.of { connection.connect() })
 		}
 	}
 
-	override fun read(callback: (Result<ByteArray, IOException>) -> Unit)
+	override fun read(coroutineScope: CoroutineScope, callback: (Result<ByteArray, IOException>) -> Unit)
 	{
-		GlobalScope.launch {
+		coroutineScope.launch {
 			callback(Result.of { connection.read() })
 		}
 	}
 
-	override fun send(data: ByteArray, callback: (Result<Unit, IOException>) -> Unit)
+	override fun send(coroutineScope: CoroutineScope, data: ByteArray, callback: (Result<Unit, IOException>) -> Unit)
 	{
-		GlobalScope.launch {
+		coroutineScope.launch {
 			callback(Result.of { connection.send(data) })
 		}
 	}
 
-	override fun close(callback: (Result<Unit, IOException>) -> Unit)
+	override fun close(coroutineScope: CoroutineScope, callback: (Result<Unit, IOException>) -> Unit)
 	{
-		GlobalScope.launch {
+		coroutineScope.launch {
 			callback(Result.of { connection.close() })
 		}
 	}

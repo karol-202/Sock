75922bf8de8af6c0532311fde91db586a5256294
karol-202 <karoljurski1@gmail.com>
2018-12-31 18:54:50 +0100

    Make converter a TransitiveBiLayer.

--- src/main/kotlin/pl/karol202/sock/converter/Converter.kt
+++ src/main/kotlin/pl/karol202/sock/converter/Converter.kt
@@ -1,23 +1,19 @@
 package pl.karol202.sock.converter
 
-import com.github.kittinunf.result.Result
-import pl.karol202.plumber.TransitiveLayer
+import pl.karol202.plumber.TransitiveBiLayer
 import pl.karol202.sock.PublicApi
 
 @PublicApi
-interface Encoder<T : Any> : TransitiveLayer<T, ByteArray>
+interface Converter<T : Any> : TransitiveBiLayer<ByteArray, T>
 {
+	override fun transform(input: ByteArray) = decode(input)
+
+	override fun transformBack(input: T) = encode(input)
+
+	@PublicApi
+	fun decode(bytes: ByteArray): T
+
 	@PublicApi
 	fun encode(data: T): ByteArray
-
-	override fun transform(input: T) = encode(input)
 }
 
-@PublicApi
-interface Decoder<T : Any> : TransitiveLayer<ByteArray, Result<T, Exception>>
-{
-	@PublicApi
-	fun decode(bytes: ByteArray): Result<T, Exception>
-
-	override fun transform(input: ByteArray) = decode(input)
-}
--- src/main/kotlin/pl/karol202/sock/converter/JsonConverter.kt
+++ src/main/kotlin/pl/karol202/sock/converter/JsonConverter.kt
@@ -1,38 +1,23 @@
 package pl.karol202.sock.converter
 
-import com.github.kittinunf.result.Result
-
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
 import pl.karol202.sock.PublicApi
 
 @PublicApi
-class JsonEncoder<T : Any>(clazz: Class<T>): Encoder<T>
+class JsonConverter<T : Any>(clazz: Class<T>) : Converter<T>
 {
 	companion object
 	{
 		@PublicApi
-		inline fun <reified T : Any> create() = JsonEncoder(T::class.java)
+		inline fun <reified T : Any> create() = JsonConverter(T::class.java)
 	}
 
 	private val adapter: JsonAdapter<T> = Moshi.Builder().build().adapter<T>(clazz)
 
 	@PublicApi
-	override fun encode(data: T) = adapter.toJson(data).toByteArray()
-}
-
-@PublicApi
-class JsonDecoder<T : Any>(clazz: Class<T>): Decoder<T>
-{
-	companion object
-	{
-		@PublicApi
-		inline fun <reified T : Any> create() = JsonDecoder(T::class.java)
-	}
-
-	private val adapter: JsonAdapter<T> = Moshi.Builder().build().adapter<T>(clazz)
+	override fun decode(bytes: ByteArray): T = adapter.fromJson(bytes.toString())!!
 
 	@PublicApi
-	override fun decode(bytes: ByteArray) =
-			Result.of<T, Exception> { adapter.fromJson(bytes.toString()) ?: throw NullPointerException() }
+	override fun encode(data: T): ByteArray = adapter.toJson(data).toByteArray()
 }

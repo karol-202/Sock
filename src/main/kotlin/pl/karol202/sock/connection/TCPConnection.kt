1447eece9f25b2a88799a7c46d7f88f450ef667d
karol-202 <karoljurski1@gmail.com>
2019-02-24 21:20:26 +0100

    Add timeout to TCPConnection.

--- src/main/kotlin/pl/karol202/sock/connection/TCPConnection.kt
+++ src/main/kotlin/pl/karol202/sock/connection/TCPConnection.kt
@@ -3,12 +3,20 @@ package pl.karol202.sock.connection
 import pl.karol202.sock.PublicApi
 import java.io.InputStream
 import java.io.OutputStream
+import java.net.InetSocketAddress
 import java.net.Socket
 
+//Timeout is in milliseconds, 0 means no timeout (infinite waiting)
 @PublicApi
 class TCPConnection(private val address: String,
-                    private val port: Int) : Connection
+                    private val port: Int,
+                    private val timeout: Int = DEFAULT_TIMEOUT) : Connection
 {
+	companion object
+	{
+		private const val DEFAULT_TIMEOUT = 1000
+	}
+
 	private class SocketConnection(val socket: Socket)
 	{
 		val inputStream: InputStream = socket.getInputStream()
@@ -25,8 +33,14 @@ class TCPConnection(private val address: String,
 	override fun connect()
 	{
 		if(isConnected) throw IllegalStateException("Already connected")
-		socketConnection = SocketConnection(Socket(address, port))
+		socketConnection = SocketConnection(createSocket())
 	}
+
+	private fun createSocket() = Socket().apply {
+		connect(createAddress(), timeout)
+	}
+
+	private fun createAddress() = InetSocketAddress(address, port)
 
 	@PublicApi
 	override fun read(): ByteArray = socketConnection?.inputStream?.readBytes() ?: throw IllegalStateException("Not connected")
